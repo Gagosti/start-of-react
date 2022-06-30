bcf32c7a2e14804b2df349a2771f7a206e549ec0
Guilherme Santos <agustinho_santos@hotmail.com>
2022-06-30 16:21:20 -0300

    react on page

---
+++ index.js
@@ -0,0 +1,6 @@
+const root = document.querySelector("#root")
+
+const t1 = React.createElement("h1", {id:"Título principal"}, "Hello World")
+const t2 = React.createElement("h4", {id:"Título principal"}, "Hello World")
+
+ReactDOM.render(element, root);
